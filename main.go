ab8488c2ca1b1212be92fe8012bb867422185fdd
Arnab Santra <arnabsantra248@gmail.com>
2026-01-29 11:46:04 +0530

    exp version

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github/Arnab-cloud/tui_weather_app/internal/ui"
 	"github/Arnab-cloud/tui_weather_app/internal/weather"
 	"log"
@@ -19,7 +18,7 @@ var VERSION = "dev"
 func main() {
 
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Print(VERSION)
+		// fmt.Print(VERSION)
 		return
 	}
 
